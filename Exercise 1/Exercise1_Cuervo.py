554f49518fcf4dd94c052e95ff61e8826d83d15f
Jewel Lois Cuervo <jccuervo@up.edu.ph>
2024-02-27 16:18:39 +0800

    Exercise 1

---
+++ Exercise 1/Exercise1_Cuervo.py
@@ -0,0 +1,5 @@
+"""
+GE 120
+Jewel Lois Cuervo
+2023-06864
+"""
---
+++ Lecture2_Cuervo.py
@@ -0,0 +1,18 @@
+print ("Kumusta Mundo")
+print (type ("Kumusta Mundo"))
+print (type(1))
+x = 2
+y = 13
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x**y)
+print(x%y)
+print(abs(y))
+print(float(y))
+print(complex(y))
+print(divmod(y,x))
+dog = ["golden retriever", "husky", "aspin", "poodle"]
+print(dog(0))
